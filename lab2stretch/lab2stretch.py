768fe89ab807799ca3de5ea57798c774b8fd0398
Amina Muumine <muumi002@umn.edu>
2020-01-31 09:58:15 -0600

    added lab2

--- lab2stretch/lab2stretch.py
+++
@@ -1,3 +0,0 @@
-weight= float(input("Enter a weight in pounds: "))
-weight= (weight * 0.454)
-print(weight,'kg')
--- lab2stretch/loan lab.py
+++
@@ -1,5 +0,0 @@
-loan= float(input("Enter loan amount: "))
-r= float(input("Enter annual interest rate: "))
-n= float(input("Enter a loan duration: "))
-payment= (r*(loan))/(1-(1 + r)**-n)
-print(payment)
