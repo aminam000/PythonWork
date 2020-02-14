5092889286c709a931316bc42254bc19f0a4f693
Amina Muumine <muumi002@umn.edu>
2020-02-14 09:55:08 -0600

    adding changes

--- labs/lab03.py
+++
@@ -1,9 +0,0 @@
-#Warm  Up
-weight_in_lbs= float(input("Enter weight in pounds: "))
-height_in_in= float(input("Enter height in inches: "))
-age= float(input("Enter age in years: "))
-gender = input("Enter M if male and F if female: ")
-if gender == "M":
-    print((66+(6.3* weight_in_lbs) + (12.9*height_in_in) - (6.8*age))/230, "chocolate bars needed to meet calorie needs")
-else:
-    print((655 +(4.3*weight_in_lbs) + (4.7*height_in_in) - (4.7*age))/230, "chocolate bars needed to meet calorie needs")
