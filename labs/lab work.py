63c2a2c37d8d784d18b2035545bdf1fa608ef3b3
muumi002 <muumi002@umn.edu>
2020-04-24 09:52:20 -0500

    Add files via upload

---
+++ labs/lab work.py
@@ -0,0 +1,17 @@
+import random
+import turtle
+def randomColor():
+    x = random.choice("red","yellow", "green", "blue", "purple", "orange")
+    print (x)
+    
+class Shape(shape):
+    def setFillcolor(str):
+    def setFilled(bool):
+    def isFilled():
+        
+class Circle:
+x = Shape(circle)
+draw(turtle)
+
+class Display:
+    def __init__(self,myt=turtle.Turtle(), scr=turtle.Screen()):
